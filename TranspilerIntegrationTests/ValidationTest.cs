eefcf42ed69a9afd1125b3546855e4dc0c0931c2
zabaweb <rafal.karwat@gmail.com>
2022-11-25 21:05:48 +0100

    Test

--- TranspilerIntegrationTests/ValidationTest.cs
+++ TranspilerIntegrationTests/ValidationTest.cs
@@ -2,19 +2,25 @@ using Xunit.Abstractions;
 
 namespace TranspilerIntegrationTests;
 
-public class validationTest
+public class ValidationTest
 {
     private readonly ITestOutputHelper _testOutputHelper;
+
+    public ValidationTest(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+
     [Fact]
     public async Task Nuget()
     {
-        var nugetDiectoryPath = @"%USERPROFILE%\.nuget\packages\";
-
+        var nugetDiectoryPath = Environment.ExpandEnvironmentVariables(@"%USERPROFILE%\.nuget\packages\");
         var nugetDirs = Directory.EnumerateDirectories(nugetDiectoryPath);
+        _testOutputHelper.WriteLine(nugetDiectoryPath);
 
         foreach(var dir in nugetDirs)
         {
-            _testOutputHelper.WriteLine(dir)
+            _testOutputHelper.WriteLine(dir);
         }
     }
 }
